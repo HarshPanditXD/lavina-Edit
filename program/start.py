b6c1d8440f4296a117883a8916cd57ce3a3d8696
Harsh Pandit <93505687+HarshPanditXD@users.noreply.github.com>
2021-12-29 01:05:28 +0530

    Update start.py

--- program/start.py
+++ program/start.py
@@ -86,6 +86,8 @@ async def start_(client: Client, message: Message):
         disable_web_page_preview=True,
     )
 
+
+
 @Client.on_message(
     command(["alive", f"alive@{BOT_USERNAME}"]) & filters.group & ~filters.edited
 )
